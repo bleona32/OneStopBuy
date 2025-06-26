1a77eb47d1c3e337c4649dc2eec58a8d3a3340d2
lizapira <lp47657@ubt-uni.net>
2025-06-26 21:02:01 +0200

    Account View Model

---
+++ ViewModels/AccountViewModel.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneStopBuy.ViewModels
+{
+    public class AccountViewModel
+    {
+        public string userId { get; set; }
+
+        public string Username { get; set; }
+
+        public string Photo { get; set; }
+
+        public string Bio { get; set; }
+
+
+    }
+
+}
