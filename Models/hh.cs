4668c221812ed878094c4887c4360b2ed80f866b
lizapira <lp47657@ubt-uni.net>
2025-06-26 20:30:07 +0200

    hhh

---
+++ Models/hh.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+/*using MongoDB.Driver;*/
+
+namespace OneStopBuy.Models
+{
+    public class News
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+    }
+}
