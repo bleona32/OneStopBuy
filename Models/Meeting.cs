819c2541ed19b689923d3c8569919b97ed948604
lizapira <lp47657@ubt-uni.net>
2025-06-26 20:50:12 +0200

    models

---
+++ Models/Meeting.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneStopBuy.Models
+{
+    public class Meeting
+    {
+        public int Id { get; set; }
+        [DataType(DataType.DateTime)]
+        public DateTime CreatedOn { get; set; }
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+        public string UserId { get; internal set; }
+    }
+}
