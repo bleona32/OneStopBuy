b2dc9312149c2b3deac59ae0c81bb5f920bf6863
lizapira <lp47657@ubt-uni.net>
2025-06-26 20:50:35 +0200

    models

---
+++ Models/UserPostLike.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneStopBuy.Models
+{
+    public class UserPostLike
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+        public int PostId { get; set; }
+
+        [ForeignKey("UserId")]
+        public ApplicationUser User { get; set; }
+
+        [ForeignKey("PostId")]
+        public Post Post { get; set; }
+    }
+}
---
+++ Models/Warranty.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneStopBuy.Models
+{
+    public class Warranty
+    {
+        public string Id { get; set; }
+
+        public string warrantyName { get; set; }
+        public int warrantyYears { get; set; }
+        public string warrantyCoverage { get; set; }
+    }
+}
