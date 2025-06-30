23087367f46a69b22260c32d191d37f5f5fd6fc4
Bleona Rrustemi <104694807+bleona32@users.noreply.github.com>
2025-06-30 19:24:05 +0200

    CreatePostViewModel

---
+++ ViewModels/CreatePostViewModel.cs
@@ -0,0 +1,18 @@
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneStopBuy.ViewModels
+{
+    public class CreatePostViewModel
+    {
+        [Required]
+        [MaxLength(100)]
+        public string Title { get; set; }
+        [Required]
+        public IFormFile Photo { get; set; }
+    }
+}
