b003396ba5225d1e12ee301538cc3061b52cf77e
Bleona Rrustemi <104694807+bleona32@users.noreply.github.com>
2025-06-30 18:32:00 +0200

    HomeController

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+using OneStopBuy.Data;
+using OneStopBuy.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OneStopBuy.Controllers
+{
+
+    /*[Authorize(Roles = "Admin")]*/
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public readonly ApplicationDbContext _context;
+        public readonly UserManager<ApplicationUser> _userManager;
+        public HomeController(ApplicationDbContext context, UserManager<ApplicationUser> userManager, ILogger<HomeController> logger)
+        {
+            _context = context;
+            _userManager = userManager;
+            _logger = logger;
+        }
+        public async Task<IActionResult> IndexMessage()
+        {
+            var currentUser = await _userManager.GetUserAsync(User);
+            if (User.Identity.IsAuthenticated)
+            {
+                ViewBag.CurrentUserName = currentUser.UserName;
+            }
+            var messages = await _context.Messages.ToListAsync();
+            return View(messages);
+        }
+
+        public async Task<IActionResult> CreateMessage(Message message)
+        {
+            if (ModelState.IsValid)
+            {
+                message.UserName = User.Identity.Name;
+                var sender = await _userManager.GetUserAsync(User);
+                message.UserID = sender.Id;
+                await _context.Messages.AddAsync(message);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("IndexMessage");
+            }
+            return Error();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+        /*public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }*/
+        /*[AllowAnonymous]*/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult NewsUpload()
+        {
+            return View();
+        }
+
+        public IActionResult allNews()
+        {
+            return View();
+        }
+
+        public IActionResult NewsReports()
+        {
+            var users = _userManager.Users;
+            return View(users);
+        }
+
+
+        /* [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+         public IActionResult Error()
+         {
+             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+         }*/
+    }
+}
